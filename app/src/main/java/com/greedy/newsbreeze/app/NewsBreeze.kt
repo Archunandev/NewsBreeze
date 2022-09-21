b91b81f3322b00d14a8c532d6e3e10916efc131b
archunandev <arjunyadav8836@gmail.com>
2022-09-21 15:58:33 +0530

    Initial commit

---
+++ app/src/main/java/com/greedy/newsbreeze/app/NewsBreeze.kt
@@ -0,0 +1,5 @@
+package com.greedy.newsbreeze.app
+
+import android.app.Application
+
+class NewsBreeze : Application()
---
+++ app/src/main/java/com/greedy/newsbreeze/ui/saved/SavedActivity.kt
@@ -0,0 +1,12 @@
+package com.greedy.newsbreeze.ui.saved
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.greedy.newsbreeze.R
+
+class SavedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saved)
+    }
+}
